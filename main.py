72f102eecc89c0ccdab88db9586182899315a07d
berkenvr <berkeonver@icloud.com>
2025-02-24 16:33:08 +0300

    defined count_events and count_repos test functions

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-import os.path
 import urllib.request
-import os
 import json
 
+event_dict =\
+    {
+        'CommitCommentEvent': 0,
+        'CreateEvent': 0,
+        'DeleteEvent': 0,
+        'ForkEvent': 0,
+        'GollumEvent': 0,
+        'IssueCommentEvent': 0,
+        'IssuesEvent': 0,
+        'MemberEvent': 0,
+        'PublicEvent': 0,
+        'PullRequestEvent': 0,
+        'PullRequestReviewEvent': 0,
+        'PullRequestReviewCommentEvent': 0,
+        'PullRequestReviewThreadEvent': 0,
+        'PushEvent': 0,
+        'ReleaseEvent': 0,
+        'SponsorshipEvent': 0,
+        'WatchEvent': 0
+    }
 
-def load_github_activity_data(username):
+
+def github_activity(username):
     BASE_URL = f'https://api.github.com/users/{username}/events'
     response = urllib.request.urlopen(BASE_URL)
 
@@ -14,37 +33,45 @@ def load_github_activity_data(username):
 
         # Overrides existing file
         with open('data.json', 'w') as f:
-            json.dump(event_data, f, indent = 4)
+            json.dump(event_data, f, indent=4)
+
+        return count_repos(event_data)
 
 
-def print_github_activity(username):
-    event_data = load_github_activity_data(username)
-"""
-CommitCommentEvent
-CreateEvent
-DeleteEvent
-ForkEvent
-GollumEvent
-IssueCommentEvent
-IssuesEvent
-MemberEvent
-PublicEvent
-PullRequestEvent
-PullRequestReviewEvent
-PullRequestReviewCommentEvent
-PullRequestReviewThreadEvent
-PushEvent
-ReleaseEvent
-SponsorshipEvent
-WatchEvent
-"""
+def count_events(event_data):
+    temp_dict = {}
+
+    for i in event_dict.keys():
+        for j in range(len(event_data)):
+            if event_data[j]['type'] == i:
+                event_dict[i] += 1
+                temp_dict.update({i: event_dict[i]})
+
+    return temp_dict
+
+
+def count_repos(event_data):
+    temp_dict = {}
+
+    for i in range(len(event_data)):
+        event_type = event_data[i]['type']
+        repo_name = event_data[i]['repo']['name']
+        event = f'{event_type} to {repo_name}'
+
+        if event not in temp_dict.keys():
+            temp_dict.update({event: 1})
+        else:
+            temp_dict.update({event: temp_dict[event] + 1})
+
+    return f'{temp_dict}'
+
 
 def main():
     print("hello world")
     try:
         while True:
             username = input('github-activity ')
-            result = print_github_activity(username)
+            result = github_activity(username)
             print(result)
     except KeyboardInterrupt:
         print('\nUser-activity cli terminated')
